5d0d231ee7d3e149293f6a1ecdef4b68f209ecc5
flbum <matt@thebrokebros.com>
2025-05-06 19:54:41 -0400

    Corrected all files

--- champ_deck/keymaps/vial/config.h
+++ champ_deck/keymaps/vial/config.h
@@ -2,13 +2,6 @@
 
 #pragma once
 
-#define VENDOR_ID     0x4642
-#define PRODUCT_ID    0x0002
-#define DEVICE_VER    0x0001
-#define MANUFACTURER  flbum
-#define PRODUCT       champ_deck
-
 #define VIAL_KEYBOARD_UID {0x35, 0xD0, 0xED, 0x5D, 0x46, 0x25, 0x80, 0x3A}
-
-#define VIAL_UNLOCK_COMBO_ROWS {0,2}
+#define VIAL_UNLOCK_COMBO_ROWS {0,0}
 #define VIAL_UNLOCK_COMBO_COLS {0,1}
--- champ_deck/keymaps/vial/keymap.c
+++ champ_deck/keymaps/vial/keymap.c
@@ -3,6 +3,11 @@
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
     [0] =   { ENCODER_CCW_CW(KC_LEFT, KC_RIGHT), ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  },
+    [1] =   { ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______)  },
+    [2] =   { ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______)  },
+    [3] =   { ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______)  }
+    
+};
 #endif
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
